019fe7f7d4cb3d7c10ffdcee82541d183bdf9fda
kyynan-4r <riskyapp11@gmail.com>
2025-06-18 23:19:35 +0700

    wokwwokwok

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,304 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <header className="fixed top-0 left-0 w-full bg-white shadow z-50 h-15">
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <div className="md:flex md:items-center md:gap-12">
+              <a className="block text-teal-600" href="#">
+                <span className="sr-only">Home</span>
+                <Image
+                  src="/home.svg"
+                  alt="Logo"
+                  width={32}
+                  height={32}
+                  className="h-8 w-auto"
+                ></Image>
+              </a>
+            </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="hidden md:block">
+              <div className=" border-gray-100 ">
+                <nav className="-mb-px flex gap-6" aria-label="Tabs">
+                  <a
+                    href="/profile.html"
+                    className="shrink-0 border-b-2 border-transparent px-10 pb-1 text-base text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                  >
+                    Beranda
+                  </a>
+                  <a
+                    href="/profile.html"
+                    className="shrink-0 border-b-2 border-transparent px-10 pb-1 text-base  text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                  >
+                    Trending ✨
+                  </a>
+
+                  <a
+                    href="/about.html"
+                    className="shrink-0 border-b-2 border-transparent px-10 pb-4 text-base text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                  >
+                    About
+                  </a>
+                </nav>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-4">
+              <button
+                type="button"
+                className="overflow-hidden rounded-full border border-gray-300 shadow-inner"
+              >
+                <span className="sr-only">Toggle dashboard menu</span>
+
+                <img
+                  src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt=""
+                  className="size-10 object-cover"
+                />
+              </button>
+
+              <div className="block md:hidden">
+                <button className="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      {/* Body */}
+      <main className="bg-sky-100 mt-6 overflow-y-hidden">
+        <div className="flex items-center justify-center w-full h-full p-10 mx-auto grid grid-cols-[1fr_2fr_1fr] gap-4">
+          {/* side menu */}
+          <div className=" h-full -ml-10 mr-30 -mt-2 border-r border-green-400 overflow-hidden">
+            <div className="px-20 py-3 -ml-15">
+              <ul className="mt-2 space-y-1">
+                <li>
+                  <h2 class="text-sm font-semibold text-gray-600">Komunitas</h2>
+                  <a
+                    href="#"
+                    className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  >
+                    Desain Grafis
+                  </a>
+                  <a
+                    href="#"
+                    className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  >
+                    Videografi
+                  </a>
+                  <a
+                    href="#"
+                    className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  >
+                    Fotografi
+                  </a>
+                  <a
+                    href="#"
+                    className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  >
+                    Menulis
+                  </a>
+                </li>
+                <h2 class="text-sm font-semibold text-gray-600 mt-4">
+                  Apa ya ini
+                </h2>
+                <li>
+                  <details className="group [&_summary::-webkit-details-marker]:hidden">
+                    <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+                      <span className="text-sm font-medium"> Teams </span>
+
+                      <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="size-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </span>
+                    </summary>
+
+                    <ul className="mt-2 space-y-1 px-4">
+                      <li>
+                        <a
+                          href="#"
+                          className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                        >
+                          Banned Users
+                        </a>
+                      </li>
+
+                      <li>
+                        <a
+                          href="#"
+                          className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                        >
+                          Calendar
+                        </a>
+                      </li>
+                    </ul>
+                  </details>
+                </li>
+
+                <li>
+                  <a
+                    href="#"
+                    className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  >
+                    Billing
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="#"
+                    className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  >
+                    Invoices
+                  </a>
+                </li>
+
+                <li>
+                  <details className="group [&_summary::-webkit-details-marker]:hidden">
+                    <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+                      <span className="text-sm font-medium"> Account </span>
+
+                      <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="size-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </span>
+                    </summary>
+
+                    <ul className="mt-2 space-y-1 px-4">
+                      <li>
+                        <a
+                          href="#"
+                          className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                        >
+                          Details
+                        </a>
+                      </li>
+
+                      <li>
+                        <a
+                          href="#"
+                          className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                        >
+                          Security
+                        </a>
+                      </li>
+
+                      <li>
+                        <a
+                          href="#"
+                          className="w-full rounded-lg px-4 py-2 [text-align:_inherit] text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                        >
+                          Logout
+                        </a>
+                      </li>
+                    </ul>
+                  </details>
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* Konten Utama */}
+          <div className="overflow-y-auto">
+            <div class="flex-1 h-full">
+              <div class="p-4 space-y-4">
+                <div class="flex justify-center items-center h-80 w-full bg-white shadow-md rounded-lg p-6">
+                  <div class="text-center">
+                    <h2 class="text-2xl font-bold text-gray-800 mb-4">
+                      Konten Card 1
+                    </h2>
+                    <p class="text-gray-600">
+                      Ini adalah konten pertama yang dapat di-scroll
+                    </p>
+                  </div>
+                </div>
+
+                <div class="flex justify-center items-center h-80 w-full bg-white shadow-md rounded-lg p-6">
+                  <div class="text-center">
+                    <h2 class="text-2xl font-bold text-gray-800 mb-4">
+                      Konten Card 2
+                    </h2>
+                    <p class="text-gray-600">
+                      Ini adalah konten kedua yang dapat di-scroll
+                    </p>
+                  </div>
+                </div>
+
+                <div class="flex justify-center items-center h-80 w-full bg-white shadow-md rounded-lg p-6">
+                  <div class="text-center">
+                    <h2 class="text-2xl font-bold text-gray-800 mb-4">
+                      Konten Card 3
+                    </h2>
+                    <p class="text-gray-600">
+                      Ini adalah konten ketiga yang dapat di-scroll
+                    </p>
+                  </div>
+                </div>
+
+                <div class="flex justify-center items-center h-80 w-full bg-white shadow-md rounded-lg p-6">
+                  <div class="text-center">
+                    <h2 class="text-2xl font-bold text-gray-800 mb-4">
+                      Konten Card 4
+                    </h2>
+                    <p class="text-gray-600">
+                      Ini adalah konten keempat yang dapat di-scroll
+                    </p>
+                  </div>
+                </div>
+
+                <div class="flex justify-center items-center h-80 w-full bg-white shadow-md rounded-lg p-6">
+                  <div class="text-center">
+                    <h2 class="text-2xl font-bold text-gray-800 mb-4">
+                      Konten Card 5
+                    </h2>
+                    <p class="text-gray-600">
+                      Ini adalah konten kelima yang dapat di-scroll
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Side bar (kanan) */}
+          <div className="h-full ml-30 mt-0 bg-white shadow-md rounded-lg"></div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
